30a2c7924217812f9bcd67d35bb6c6e8eede5994
rahman-anika <anikarahman37@gmail.com>
2021-11-08 01:01:25 +0600

    few modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const cors = require('cors');
 const admin = require("firebase-admin");
 require('dotenv').config();
